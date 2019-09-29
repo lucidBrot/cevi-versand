95ad4edc039c14949efec9c5f4c95b569f0a1c74
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-09-29 15:24:20 +0200

    why need lifetime?

--- pdfgen/src/main.rs
+++ pdfgen/src/main.rs
@@ -87,8 +87,8 @@ fn draw_sidebadges (current_layer: &printpdf::PdfLayerReference,
     let mut y = start_y;
     for (num, text_ref) in texts.iter().enumerate() {
 //        let text = format!("{} {}", numbers[num], text_ref);
-        let tmp = pluralizable::Text{"single", "taken"};
-        let text = tmp.for(numbers[num]);
+        let tmp = pluralizable::Text::new("single", "taken");
+        let text = tmp.for_num(numbers[num]);
         draw_sidebadge(&current_layer, start_x, y,
                    &font, font_size, &text);
         y += badge_spacing_y;
