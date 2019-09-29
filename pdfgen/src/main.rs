8388990ed34a115677aea13d5bd032c2acdc5e31
Eric Mink <eric@mink.li>
2019-09-29 20:35:12 +0200

    Turns out that all I needed was to put the other file in scope

--- pdfgen/src/main.rs
+++ pdfgen/src/main.rs
@@ -89,7 +89,7 @@ fn draw_sidebadges (current_layer: &printpdf::PdfLayerReference,
     for (num, text_ref) in texts.iter().enumerate() {
 //        let text = format!("{} {}", numbers[num], text_ref);
         let tmp: pluralizable::Text = pluralizable::Text::new("single", "taken");
-        let text: String = pluralizable::Pluralizable::for_num(&tmp, numbers[num]);
+        let text: String = tmp.for_num(numbers[num]);
         draw_sidebadge(&current_layer, start_x, y,
                    &font, font_size, &text);
         y += badge_spacing_y;
