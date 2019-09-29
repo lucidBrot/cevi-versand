55790fb1c589e9bb03e2aa44170b1c7a831d2b12
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-09-29 15:48:53 +0200

    new not found

--- pdfgen/src/main.rs
+++ pdfgen/src/main.rs
@@ -88,7 +88,7 @@ fn draw_sidebadges (current_layer: &printpdf::PdfLayerReference,
     for (num, text_ref) in texts.iter().enumerate() {
 //        let text = format!("{} {}", numbers[num], text_ref);
         let tmp = pluralizable::Text::new("single", "taken");
-        let text = tmp.for_num(numbers[num]);
+        let text: String = tmp.for_num(numbers[num]);
         draw_sidebadge(&current_layer, start_x, y,
                    &font, font_size, &text);
         y += badge_spacing_y;
