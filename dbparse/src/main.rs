b50bc1fdcd70861aa51948a3007e090cf9131f21
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-10-26 11:28:41 +0200

    failing to deserialize

--- dbparse/src/main.rs
+++ dbparse/src/main.rs
@@ -65,5 +65,56 @@ fn get_data_sorted_by_address (db_conf : &DB_Conf) -> Result<String, reqwest::Er
     .text()?;
     println!("body = {:?}", body);
     // TODO: deserialize, into what?
+    let deserialized: serde_json::Value = serde_json::from_str(&body).unwrap();
+    let people: Vec<Person> = serde_json::from_value(deserialized.get("people").unwrap().clone()).unwrap();
     Ok(body)
+}
+
+/*
+ * JSON from sorted by address:
+ *
+  "people":[ 
+      { 
+         "id":"18389",
+         "type":"people",
+         "href":"https://db.cevi.ch/groups/1341/people/18389.json",
+         "first_name":"Leon Jonas",
+         "last_name":"Roth",
+         "nickname":"Takka",
+         "company_name":"",
+         "company":false,
+         "email":"pfarramt.roth@gmail.com",
+         "address":"Am Burenbüel 28",
+         "zip_code":"8320",
+         "town":"Fehraltorf",
+         "country":"CH",
+         "picture":"https://db.cevi.ch/assets/profil-3a8452c9ac8e8b1b70b9d4f4250417bea5be8a4518dbfae44db944f8fda07ca5.png",
+         "salutation_parents":"",
+         "name_parents":"Barbara und Martin",
+         "links":{ 
+            "ortsgruppe":"115",
+            "phone_numbers":[ 
+               "22487",
+               "28203",
+               "28204"
+            ],
+            "roles":[ 
+               "54716"
+            ]
+         }
+ *
+ */
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    id: usize,
+    href: String,
+    first_name: String,
+    last_name: String,
+    nickname: String,
+    email: String,
+    address: String,
+    zip_code: String,
+    town: String,
+    name_parents: String,
+    roles: Vec<usize>,
 }
