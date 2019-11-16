7ccdc23d9cda9072b6e03eb0a4d7bed13235cdab
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-11-16 13:58:38 +0100

    mismatched types

--- combine/src/main.rs
+++ combine/src/main.rs
@@ -22,12 +22,44 @@ fn main() {
 }
 
 
-fn merge_households( people: &Vec<dbparse::ReasonablePerson>,
-                     mapping: &dbparse::mapping::GroupMapping) {
+fn merge_households<'b>( people: &'b Vec<dbparse::ReasonablePerson>,
+                     mapping: &dbparse::mapping::GroupMapping) -> Vec<pdfgen::CouvertInfo<'b>> {
     // TODO: first replace strasse and str
     // sort by (dorf, street, last name ....)
     // merge same into one couvert info
     // store single ones into one couvert info
     // return couvert infos
+    for person in people {
+        /* Person
+        first_name: String
+        last_name: String
+        nickname: String
+        address: String
+        zip_code: String
+        town: String
+        name_parents: String
+        roles: HashSet<Role>
+        groups: HashSet<ReasonableGroup>*/
+
+        let normalized_address = normalize_address(person.address);
+    }
+
+}
+
+/// removes newlines within address
+/// trims starting and ending whitespace
+/// replaces "str."  with "strasse" and replaces " str." with "Strasse"
+/// ```
+/// let addr : String = String::from(" add\nressstr.  ");
+/// let normalized : String = normalize_address(addr);
+/// assert_eq!(normalized, String::from("addressstrasse"))
+/// ```
+fn normalize_address(address: String) -> String {
     
+}
+
+/// replaces Pfäffikon, Pfaeffikon, etc with "Pfäffikon ZH"
+/// TODO: documentation test
+fn normalize_town(town: String) -> String {
+
 }
