e4bf4558312fe8e05f71629cad983ea450e4c934
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-11-17 09:54:02 +0100

    more thoughts

--- combine/src/roletranslation.rs
+++ combine/src/roletranslation.rs
@@ -4,3 +4,7 @@
 // auto-map some roles to be ignored, so that only reasonable roles remain
 // create yaml file so user can specify what other roles should map to what, and what roles to
 // ignore
+//
+//
+// Or perhaps simply ignore everything that is not obviously
+// Leiter/Teilnehmer/Traegerkreis/Ehemalig. No user interaction required.
