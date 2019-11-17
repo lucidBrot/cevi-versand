8ccd8aa7086680564c0de9d950e410cc5b22c836
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-11-17 09:50:09 +0100

    start thinking about roles

---
+++ combine/src/roletranslation.rs
@@ -0,0 +1,6 @@
+
+// create map of all roles from db
+// auto-map to roles from pdfgen (Leiter, Teilnehmer, Traegerkreis, Ehemalig)
+// auto-map some roles to be ignored, so that only reasonable roles remain
+// create yaml file so user can specify what other roles should map to what, and what roles to
+// ignore
