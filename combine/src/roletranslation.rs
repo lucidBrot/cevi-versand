639487eb43a86cbe2ceea622036c8509d1b2a0fa
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-11-17 13:38:41 +0100

    ..

--- combine/src/roletranslation.rs
+++ combine/src/roletranslation.rs
@@ -11,14 +11,20 @@ use pdfgen;
 // Leiter/Teilnehmer/Traegerkreis/Ehemalig. No user interaction required.
 //
 pub fn role_to_role(dbrole: &dbparse::Role) -> pdfgen::Role{
-    
-    return pdfgen::Role::Traegerkreis;
+
+    match dbrole.role_type.as_ref() { // TODO: test with whole database
+        "Teilnehmer/-in" => pdfgen::Role::Teilnehmer,
+        "Traegerkreis" => pdfgen::Role::Traegerkreis, // TODO: how to recognize trägerkreis?
+        "Minigruppenleiter/-in" => pdfgen::Role::Leiter,
+        // TODO: case where it did not match
+        &_ => println!("r2r: don't know what to do with {}", _),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_role_to_role_sample() {
+    fn test_role_to_role_teilnehmer() {
         use std::rc::Rc;
 
         let db_role = dbparse::Role::new(
@@ -36,5 +42,31 @@ mod tests {
         let pdf_role: pdfgen::Role = super::role_to_role(&db_role);
 
         assert_eq!(pdf_role, pdfgen::Role::Teilnehmer);
+    }
+
+    #[test]
+    fn test_role_to_role_unexpected() {
+        use std::rc::Rc;
+
+        let db_role = dbparse::Role::new(
+            /*id: Rc<str>,
+            pub role_type: String,
+            label: Option<String>,
+            links: RoleLinks,*/
+            std::rc::Rc::<str>::from("test_id"),
+            String::from("etwas unerwartets"),
+            None,
+            String::from("Fake group id"),
+            String::from("fake layer group"),
+        );
+        
+        let pdf_role: pdfgen::Role = super::role_to_role(&db_role);
+
+        assert_eq!(pdf_role, pdfgen::Role::Teilnehmer);
+    }
+
+    #[test]
+    fn test_role_to_role_traegerkreis() {
+        assert!(false, "please implement");
     }
 }
