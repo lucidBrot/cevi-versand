719e2e66dbc343e099beebe9ce8d93a877c9811e
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-11-25 22:43:08 +0100

    gui: todo

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,6 +1,11 @@
+mod support;
 use conrod_example_shared::{WIN_W, WIN_H};
 use conrod_glium::Renderer;
 use glium::Surface;
+
+fn main() {
+    // TODO: steal code from conrod_glium/examples
+}
 
 
 #[cfg(test)]
