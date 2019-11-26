79f63b41417fbde86b029fe31e2be60dac7b5852
Eric Mink <eric@mink.li>
2019-11-26 19:33:51 +0100

    hello world builds

--- gui/HowToConrod.md
+++ gui/HowToConrod.md
@@ -24,7 +24,8 @@ members = [
 ```
 ```bash
 cargo build # succeeds
-cargo run --example all_winit_glium
+cargo run --example all_winit_glium # cool example
+cargo run --example hello_world # simple example
 ```
 
 examples are found in `backends/conrod_glium`.
@@ -37,5 +38,137 @@ Their examples are made to be run _within_ their crate. We want to get this to r
 # cd SOMEWHERE
 # create a new crate called gui
 cargo new --lib gui
+```
+
+Have a look at `conrod/backends/conrod_glium/hello_world.rs`. It's written in edition 2015 I think, so some syntax in there is no longer encouraged today. It should still compile though.
+
+Let us start with the first few lines of `hello_world.rs`:
+
+```rust
+//! A simple example that demonstrates using conrod within a basic `winit` window loop, using
+//! `glium` to render the `conrod_core::render::Primitives` to screen.
+
+#[macro_use] extern crate conrod_core;
+extern crate conrod_glium;
+#[macro_use] extern crate conrod_winit;
+extern crate find_folder;
+extern crate glium;
+
+mod support;
+
+use conrod_core::{widget, Colorable, Positionable, Widget};
+use glium::Surface;
+
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 200;
+```
+
+I copied them over to my own crate in `gui/src/lib.rs` and try to build that.
+
+```bash
+cargo build
+# fails because it can't find crate "conrod_core"
+```
+
+The `extern crate` is no longer needed in the 2018 edition though:
+
+>  Now, to add a new crate to your project, you can add it to your `Cargo.toml`, and then there is no step two. If you're not using Cargo, you already had to pass `--extern` flags to give `rustc` the location of external crates, so you'd just keep doing what you were doing there as well. 
+>
+> [Source]( https://doc.rust-lang.org/edition-guide/rust-2018/module-system/path-clarity.html#an-exception )
+
+So let us depend on conrod by adding the line to `Cargo.toml`:
+
+```bash
+$ cat Cargo.toml
+[package]
+name = "gui"
+version = "0.1.0"
+authors = ["eric <eric@mink.li>"]
+edition = "2018"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[dependencies]
+conrod = { version = "0.61", features = ["winit", "glium"] }
+```
+
+That does not help finding `conrod_core`. Luckily they have a hidden [third chapter]( https://docs.piston.rs/conrod/src/conrod_core/guide/chapter_3.rs.html ) to their guide (from 2017), which is not linked in the README. And it turns out that we need to do quite a few things differently when we are not within the `conrod` crate. So let's follow that guide instead.
+
+> For simplicity, I'm
+> specifying the backend in `cargo.toml` because we then won't have to think
+> about it in the code itself.
+
+> I'm using the glium backend. [Glium](https://github.com/glium/glium) is a
+> cross-platform, safe wrapper for OpenGL, written in Rust. It is listed as not-
+> maintained on Github, but limited maintenance is being undertaken by the
+> developer of Conrod. Glium has a very good, gentle
+> [introduction]( https://github.com/glium/glium/tree/master/book ) to OpenGL - enough to
+> convince me that I really want Conrod to do the work for me!
+
+After following the guide up to line 196, we have a file that actually builds with our one dependency in `Cargo.toml` above.
+
+```rust
+/// gui/src/lib.rs
+/// A Hello World based and annotated with help of https://docs.piston.rs/conrod/src/conrod_core/guide/chapter_3.rs.html
+use conrod::backend::glium::glium;
+/*
+ `Surface` is a trait required for glium, specifically for the call to
+`target.clear_color` which is coming later.
+ */
+use glium::Surface;
+
+/*
+ The first chunk of boilerplate creates an event loop, which will handle
+interaction with the UI, then a window, then a context, then finally links the
+event loop, window and context together into a display. The display is the
+home for the UI, and is an OpenGL context provided by glium.
+*/
+
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 200;
+const TITLE: &str = "Hello Conrod!";
+
+
+fn main() {
+    // Build the window.
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new()
+        .with_title(TITLE)
+        .with_dimensions((WIDTH, HEIGHT).into());
+    let context = glium::glutin::ContextBuilder::new()
+        .with_vsync(true)
+        .with_multisampling(4);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    /*
+       Now create the UI itself. Conrod has a builder that contains and looks after
+       the UI for the user.
+       */
+    let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+
+    /*
+       Conrod can use graphics. It stores these in a map. The system needs the map,
+       even though it doesn't contain anything at this time, so create it:
+       */
+    let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+
+    /*
+       Finally, Conrod needs to render its UI. It uses a renderer to do this, so
+       create one:
+       */
+    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+
+    'render: loop {
+        // Draw the UI if it has changed
+        if let Some(primitives) = ui.draw_if_changed() {
+            renderer.fill(&display, primitives, &image_map);
+            let mut target = display.draw();
+            target.clear_color(0.0, 1.0, 0.0, 1.0);
+            renderer.draw(&display, &mut target, &image_map).unwrap();
+            target.finish().unwrap();
+        }
+    }
+}
+
 ```
 
--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,112 +1,60 @@
-mod support;
-use conrod_example_shared::{WIN_W, WIN_H};
-use conrod_glium::Renderer;
+/// A Hello World based and annotated with help of https://docs.piston.rs/conrod/src/conrod_core/guide/chapter_3.rs.html
+use conrod::backend::glium::glium;
+/*
+ `Surface` is a trait required for glium, specifically for the call to
+`target.clear_color` which is coming later.
+ */
 use glium::Surface;
+
+/*
+ The first chunk of boilerplate creates an event loop, which will handle
+interaction with the UI, then a window, then a context, then finally links the
+event loop, window and context together into a display. The display is the
+home for the UI, and is an OpenGL context provided by glium.
+*/
+
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 200;
+const TITLE: &str = "Hello Conrod!";
+
 
 fn main() {
     // Build the window.
     let mut events_loop = glium::glutin::EventsLoop::new();
     let window = glium::glutin::WindowBuilder::new()
-        .with_title("Conrod with glium!")
-        .with_dimensions((WIN_W, WIN_H).into());
+        .with_title(TITLE)
+        .with_dimensions((WIDTH, HEIGHT).into());
     let context = glium::glutin::ContextBuilder::new()
         .with_vsync(true)
         .with_multisampling(4);
     let display = glium::Display::new(window, context, &events_loop).unwrap();
-    let display = support::GliumDisplayWinitWrapper(display);
 
-    // Construct our `Ui`.
-    let mut ui = conrod_core::UiBuilder::new([WIN_W as f64, WIN_H as f64]).theme(conrod_example_shared::theme()).build();
+    /*
+       Now create the UI itself. Conrod has a builder that contains and looks after
+       the UI for the user.
+       */
+    let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
 
-    // The `widget::Id` of each widget instantiated in `conrod_example_shared::gui`.
-    let ids = conrod_example_shared::Ids::new(ui.widget_id_generator());
+    /*
+       Conrod can use graphics. It stores these in a map. The system needs the map,
+       even though it doesn't contain anything at this time, so create it:
+       */
+    let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
 
-    // Add a `Font` to the `Ui`'s `font::Map` from file.
-    let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
-    let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
-    ui.fonts.insert_from_file(font_path).unwrap();
+    /*
+       Finally, Conrod needs to render its UI. It uses a renderer to do this, so
+       create one:
+       */
+    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
 
-    // Load the Rust logo from our assets folder to use as an example image.
-    fn load_rust_logo(display: &glium::Display) -> glium::texture::Texture2d {
-        let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
-        let path = assets.join("images/rust.png");
-        let rgba_image = image::open(&std::path::Path::new(&path)).unwrap().to_rgba();
-        let image_dimensions = rgba_image.dimensions();
-        let raw_image = glium::texture::RawImage2d::from_raw_rgba_reversed(&rgba_image.into_raw(), image_dimensions);
-        let texture = glium::texture::Texture2d::new(display, raw_image).unwrap();
-        texture
-    }
-
-    let mut image_map = conrod_core::image::Map::new();
-    let rust_logo = image_map.insert(load_rust_logo(&display.0));
-
-    // A demonstration of some app state that we want to control with the conrod GUI.
-    let mut app = conrod_example_shared::DemoApp::new(rust_logo);
-
-    // A type used for converting `conrod_core::render::Primitives` into `Command`s that can be used
-    // for drawing to the glium `Surface`.
-    //
-    // Internally, the `Renderer` maintains:
-    // - a `backend::glium::GlyphCache` for caching text onto a `glium::texture::Texture2d`.
-    // - a `glium::Program` to use as the shader program when drawing to the `glium::Surface`.
-    // - a `Vec` for collecting `backend::glium::Vertex`s generated when translating the
-    // `conrod_core::render::Primitive`s.
-    // - a `Vec` of commands that describe how to draw the vertices.
-    let mut renderer = Renderer::new(&display.0).unwrap();
-
-    // Start the loop:
-    //
-    // - Poll the window for available events.
-    // - Update the widgets via the `conrod_example_shared::gui` fn.
-    // - Render the current state of the `Ui`.
-    // - Repeat.
-    let mut event_loop = support::EventLoop::new();
-    'main: loop {
-
-        // Handle all events.
-        for event in event_loop.next(&mut events_loop) {
-
-            // Use the `winit` backend feature to convert the winit event to a conrod one.
-            if let Some(event) = support::convert_event(event.clone(), &display) {
-                ui.handle_event(event);
-                event_loop.needs_update();
-            }
-
-            match event {
-                glium::glutin::Event::WindowEvent { event, .. } => match event {
-                    // Break from the loop upon `Escape`.
-                    glium::glutin::WindowEvent::CloseRequested |
-                    glium::glutin::WindowEvent::KeyboardInput {
-                        input: glium::glutin::KeyboardInput {
-                            virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
-                            ..
-                        },
-                        ..
-                    } => break 'main,
-                    _ => (),
-                },
-                _ => (),
-            }
-        }
-
-        // Instantiate a GUI demonstrating every widget type provided by conrod.
-        conrod_example_shared::gui(&mut ui.set_widgets(), &ids, &mut app);
-
-        // Draw the `Ui`.
+    'render: loop {
+        // Draw the UI if it has changed
         if let Some(primitives) = ui.draw_if_changed() {
-            renderer.fill(&display.0, primitives, &image_map);
-            let mut target = display.0.draw();
-            target.clear_color(0.0, 0.0, 0.0, 1.0);
-            renderer.draw(&display.0, &mut target, &image_map).unwrap();
+            renderer.fill(&display, primitives, &image_map);
+            let mut target = display.draw();
+            target.clear_color(0.0, 1.0, 0.0, 1.0);
+            renderer.draw(&display, &mut target, &image_map).unwrap();
             target.finish().unwrap();
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
