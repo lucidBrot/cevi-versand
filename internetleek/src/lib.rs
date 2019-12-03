02e58a389c87c03735d9a5719f545f9715a43028
Eric Mink <eric@mink.li>
2019-12-03 20:27:12 +0100

    only provide CliInternetLeek on not wasm

--- internetleek/src/lib.rs
+++ internetleek/src/lib.rs
@@ -3,10 +3,17 @@ pub trait InternetLeek {
     fn GET_body (url: &str) -> Result<String, Box<dyn std::error::Error>>;
 }
 
-pub struct CliInternetLeek {
-
+#[cfg(not(target_arch = "asm32"))]
+pub struct CliInternetLeek {}
+#[cfg(not(target_arch = "asm32"))]
+impl CliInternetLeek {
+    fn new() -> Self {
+        CliInternetLeek{}
+    }
 }
 
+
+#[cfg(not(target_arch = "asm32"))]
 impl InternetLeek for CliInternetLeek {
     fn GET_body (url: &str) -> Result<String, Box<dyn std::error::Error>>  {
         Ok(chttp::get(url)?.into_body().text()?)
