e24caf37a5dd8b67e4bb3e2ac6a49c464e347283
Eric Mink <eric@mink.li>
2019-12-11 21:12:22 +0100

    add output for merging

--- combine/src/injection.rs
+++ combine/src/injection.rs
@@ -12,12 +12,13 @@ pub fn inject_couvert_infos(couvert_infos: &mut Vec<CouvertInfo>) {
                 Receiver {
                     nickname: "Herbert".to_string(),
                     group: "Herbert Fan Club".to_string(),
-                    role: Role::Custom("Herbteilungsleiter".to_string()),
+                    role: Role::Custom("Debug".to_string()),
                 },
             ],
 
         }
     ];
+    println!("Injected {} people after merging", drug.len());
 
     couvert_infos.append(&mut drug);
 }
