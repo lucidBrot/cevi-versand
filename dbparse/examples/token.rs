e620ff7f72f8b4fbd560347601ae8d7ec2ff022d
Eric Mink <eric@mink.li>
2019-12-22 15:54:31 +0100

    TODO: get rid of login_name in config.yaml?

--- dbparse/examples/token.rs
+++ dbparse/examples/token.rs
@@ -1,12 +1,12 @@
 fn main() {
     print!("e-mail: ");
     let mut input_email = String::new();
-    let _bytes_read = std::io::stdin().read_line(&mut input).expect("Non-utf8 string input!");
+    let _bytes_read = std::io::stdin().read_line(&mut input_email).expect("Non-utf8 string input!");
     print!("passwort: ");
     let mut input_pass = String::new();
-    let _bytes_read = std::io::stdin().read_line(&mut input).expect("Non-utf8 string input!");
+    let _bytes_read = std::io::stdin().read_line(&mut input_pass).expect("Non-utf8 string input!");
 
     let token = dbparse::get_auth_token(input_email, input_pass);
-    println!("token: {}", token);
+    println!("token: {}", token.unwrap());
 
 }
