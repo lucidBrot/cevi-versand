f1567bacf42d4c21762deb304255c778bb458eed
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-12-23 11:10:56 +0100

    .

--- dbparse/examples/token.rs
+++ dbparse/examples/token.rs
@@ -1,16 +1,18 @@
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     use std::io::Write;
     print!("e-mail: ");
-    std::io::stdout().flush();
+    std::io::stdout().flush()?;
     let mut input_email = String::new();
     let _bytes_read = std::io::stdin().read_line(&mut input_email).expect("Non-utf8 string input!");
-    print!("passwort: ");
-    std::io::stdout().flush();
-    let mut input_pass = String::new();
-    let _bytes_read = std::io::stdin().read_line(&mut input_pass).expect("Non-utf8 string input!");
 
-    let token = dbparse::get_auth_token(input_email.trim().as_ref(), input_pass.trim().as_ref());
+    print!("Type a pass: ");
+    let pass = "TODO".to_string(); //passwd::read_password(); // TODO: how to read password without displaying it?
+    println!("Your password is {}", pass);
+    std::io::stdout().flush()?;
+
+    let token = dbparse::get_auth_token(input_email.trim().as_ref(), pass.trim().as_ref());
     // TODO: trim will be a problem if the password contains whitespace
     println!("token: {}", token.unwrap());
+    Ok(())
 
 }
