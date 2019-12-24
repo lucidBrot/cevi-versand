f19ccfc2283bdd63ccd7c4036cbf6f486bf92284
Eric Mink <eric@mink.li>
2019-12-24 15:33:29 +0100

    implement ui::interactively_get_auth_token

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -8,6 +8,8 @@ pub trait UserInteractor {
     fn on_pdf_generation_finished(&self) {}
     fn error_missing_config_file(&self, _filename: String) {}
     fn error_injecting_couverts(&self, _error: &dyn std::error::Error) {}
+    fn error_fetching_auth_token(&self, _error: &std::io::Error) {}
+    fn interactively_get_auth_token(&self) -> Result<String, std::io::Error>;
 }
 
 /// Simplistic default user interface
@@ -50,5 +52,48 @@ UI: There was an error while trying to inject additional people:
 "###,
             error
         );
+    }
+
+    fn error_fetching_auth_token(&self, error: &std::io::Error) {
+        println!("Problem fetching token: {:?}", error);
+        match error.kind() {
+            std::io::ErrorKind::Other => println!("Perhaps your internet connection is down?"),
+            std::io::ErrorKind::InvalidData => {
+                println!("Perhaps the credentials were wrong, perhaps the database is down, ...")
+            },
+            _ => (),
+        }
+    }
+
+    fn interactively_get_auth_token(&self) -> Result<String, std::io::Error> {
+        use std::io::Write;
+        print!("e-mail: ");
+        std::io::stdout().flush()?;
+        let mut input_email = String::new();
+        let _bytes_read = std::io::stdin()
+            .read_line(&mut input_email)
+            .expect("Non-utf8 string input!");
+
+        print!("Type a pass: ");
+        std::io::stdout().flush()?;
+        let mut pass = String::new();
+        let _bytes_read = std::io::stdin()
+            .read_line(&mut pass)
+            .expect("Non-utf8 string input for password!");
+        // TODO: how to read password without displaying it?
+        std::io::stdout().flush()?;
+
+        let auth_token = dbparse::get_auth_token(input_email.trim().as_ref(), pass.trim().as_ref());
+        match auth_token {
+            Err(e) => {
+                self.error_fetching_auth_token(&e);
+                Err(e)
+            },
+            Ok(token) => {
+                // TODO: trim will be a problem if the password contains whitespace
+                println!("Auth Token: {:?}", token);
+                Ok(token.to_string())
+            },
+        }
     }
 }
