39b596dcb756f8043942c8a88ea7b495bbdaf7fa
U-FRIENDLY_NAME\eric <eric@mink.li>
2020-01-04 22:27:41 +0100

    reproduce problem in conflict error message

---
+++ debugcli/src/main.rs
@@ -0,0 +1,24 @@
+extern crate clap;
+use clap::*;
+
+#[derive(Clap)]
+struct Opts {
+    /// Print debug info
+    #[clap(short = "d", conflicts_with = "not_debug")]
+    debug: bool,
+    /// Explicitly disable debug printing
+    #[clap(short = "D", conflicts_with = "debug")]
+    not_debug: bool,
+
+}
+
+fn main() {
+    let t: Opts = Opts::parse();
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+            if t.debug { println!("testing with debug output on."); }
+            if t.not_debug { println!("testing with debug output off."); }
+            if t.debug && t.not_debug { println!("WTF!"); }
+            if !t.debug && !t.not_debug { println!("Defaulting to testing with debug output off"); }
+    }
