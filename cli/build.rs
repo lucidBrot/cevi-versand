730404ec5a275976086c40b857d270d5a6051522
Eric Mink <eric@mink.li>
2020-01-05 16:59:42 +0100

    build on unix?

--- cli/build.rs
+++ cli/build.rs
@@ -1,5 +1,7 @@
+#[cfg(windows)]
 extern crate winres;
 
+#[cfg(windows)]
 fn main() {
   if cfg!(target_os = "windows") {
     let mut res = winres::WindowsResource::new();
@@ -7,3 +9,6 @@ fn main() {
     res.compile().unwrap();
   }
 }
+
+#[cfg(unix)]
+fn main(){}
